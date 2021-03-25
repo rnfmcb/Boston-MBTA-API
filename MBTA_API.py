a7dfd42a85c08650d3184561a1b146757bba7c47
Rachel <rachelfestervand@amiwx.com>
2021-03-25 00:39:09 +0000

    Started project, created variables

---
+++ MBTA_API.py
@@ -0,0 +1,9 @@
+#Python program that takes in two stops that are on the same route and can predict the next vehicle that can take you to the second stop. 
+
+
+route = input("What is your route?"); 
+stop1 = input("What is your first stop"); 
+stop2 = input("What is your second stop"); 
+
+
+
